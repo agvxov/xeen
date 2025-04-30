e294724e3c6304d0dcf312806f10c8b7a2b59ea6
xolatile <xolatile@proton.me>
2025-04-30 18:56:34 +0200

    Removed exporter, do nott touch anything yet...

--- source/exporter.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXPORTER_H
-#define EXPORTER_H
-
-extern signed export_png_image(const char     * path,
-                               const unsigned * data,
-                                     unsigned   width,
-                                     unsigned   height);
-
-#endif
