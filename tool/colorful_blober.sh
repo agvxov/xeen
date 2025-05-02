33baa7d69bd70dd155a5027d0acdf8d3d3dcaf14
anon <anon@anon.anon>
2025-05-02 16:54:42 +0200

    background support

--- tool/colorful_blober.sh
+++ tool/colorful_blober.sh
@@ -1,1 +1,1 @@
-echo -e "\033[45m\033[1mXXX:\033[0m \033[34m\033[1this\033[0;34m sentence is \033[31;44mcolorful\033[0m"
+echo -e "\033[45mXXX:\033[0m \033[34m\033[1mthis\033[0;34m sentence is \033[31;44mcolorful\033[0m"
