886c46d415846bf15ab374b7a1de75466f4dad8e
xolatile <xolatile@proton.me>
2025-05-03 15:48:12 +0200

    Font loading revisited...

--- source/ttf_quadruplet.c
+++ source/ttf_quadruplet.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <fts.h>
 #include <alloca.h>
@@ -18,9 +19,12 @@ int ttf_style(const char * name) {
     return r;
 }
 
-ttf_quadruplet_t load_font(const char * target_name) {
-    const char * const fonts_path = "/usr/share/fonts/"; // XXX
+ttf_quadruplet_t load_font(char * fonts_path, const char * target_name) {
     ttf_quadruplet_t r = (ttf_quadruplet_t){0};
+
+    if (fonts_path == NULL) {
+        fonts_path = strdup("/usr/share/fonts/TTF");
+    }
 
     char * paths[] = {strdup(fonts_path), NULL};
     FTS *tree = fts_open(paths, FTS_PHYSICAL | FTS_NOCHDIR, NULL);
@@ -36,21 +40,25 @@ ttf_quadruplet_t load_font(const char * target_name) {
 
         const char * base = entry->fts_name;
 
-        char * lower_name = alloca(strlen(base));
+        char * lower_name = alloca(strlen(base) + 1);
         for (int i = 0; base[i] != '\0'; i++) {
             lower_name[i] = tolower(base[i]);
         }
+        lower_name[strlen(base)] = '\0';
 
         if (!strstr(lower_name, target_name)) {
             continue;
         }
 
+        char buf [1024] = "";
+
         int style = ttf_style(lower_name);
+        snprintf(buf, 1024, "%s/%s", fonts_path, base);
         switch (style) {
-            case TTF_NORMAL:     r.normal     = strdup(base); break;
-            case TTF_BOLD:       r.bold       = strdup(base); break;
-            case TTF_ITALIC:     r.italic     = strdup(base); break;
-            case TTF_BOLDITALIC: r.bolditalic = strdup(base); break;
+            case TTF_NORMAL:     r.normal     = strdup(buf); break;
+            case TTF_BOLD:       r.bold       = strdup(buf); break;
+            case TTF_ITALIC:     r.italic     = strdup(buf); break;
+            case TTF_BOLDITALIC: r.bolditalic = strdup(buf); break;
         }
 
         if (is_quadruplet_full(r)) { break; }
